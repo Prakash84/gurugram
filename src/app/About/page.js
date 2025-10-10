19e99f2db5583e7307eca3d00ef467f81542367b
LAPTOP-8C9L3FQT\Jitender chouhan <prakastiwarichs@gmail.com>
2025-10-10 14:04:47 +0530

    Updated something

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -2,8 +2,8 @@ import Abpage from "./Abpage";
 // src/app/page.js
 // <-- NO "use client" here -->
 export const metadata = {
-  title: "Best Spa in Delhi For Complete Relaxation | Spa Delhi Offer @1999",
-  description: "Looking for a relaxing Spa in Delhi? Experience the best full body, sandwich massage, Couple massages at the top massage parlour in Delhi. Book Your Session Now.",
+  title: "About Our Luxury Spa in Gurgaon | 5 star outlets in Delhi Ncr",
+  description: "Know more about our luxury spa in Gurgaon, offering body spa, couple spa, and ayurvedic therapies to clients across Delhi NCR with top-class service.",
   openGraph: {
     title: "Get Best Body Massage in Delhi | First Visit Offer 1999",
     description: "Looking for a relaxing massage in Delhi? Experience the best full body, sandwich massage, Couple massages at the top massage parlour in Delhi.",
